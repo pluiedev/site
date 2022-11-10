667af565553bd46085dd72237bcb70e3f162e759
leocth <22406910+LeoCTH@users.noreply.github.com>
2022-11-10 08:46:18 +0800

    better email modal :3

--- src/assets/scripts/email_modal.js
+++ src/assets/scripts/email_modal.js
@@ -1,14 +1,20 @@
-function copyEmail() {
-    const email = document.getElementById("obscured-email");
-    const sanitized = email.textContent.replace(/\d/g, "");
+(() => {
+	const button = document.getElementById("copy-email");
 
-    navigator.clipboard.writeText(sanitized).then(
-        () => {
-            alert("Successfully copied address to clipboard!");
-        },
-        () => {
-            alert("Failed to copy to clipboard!");
-        }
-    );
-}
-document.getElementById("copy-email").onclick = copyEmail;
+	function conclude(success) {
+		button.classList.remove("is-loading");
+		button.dataset.success = success;
+		setTimeout(() => (button.dataset.success = ""), 1500);
+	}
+	button.onclick = () => {
+		button.classList.add("is-loading");
+
+		const email = document.getElementById("obscured-email");
+		const sanitized = email.textContent.replace(/\d/g, "");
+
+		navigator.clipboard.writeText(sanitized).then(
+			() => conclude(true),
+			() => conclude(false)
+		);
+	};
+})();
