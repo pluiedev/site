0ef679aa074c9b096d0782f80beab41e38c74050
Leah Amelia Chen <hi@pluie.me>
2026-07-07 05:02:50 +0800

    flake: update, unfuck deno.json
    idk why there were two compilerOptions

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Source code of pluie.me";
 
-  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.xz";
 
   outputs = {
     self,
