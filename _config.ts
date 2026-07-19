667ae7e9ec515cbc5b5ed56007048bbda9183f1f
Leah Amelia Chen <hi@pluie.me>
2026-07-20 01:01:24 +0800

    config: add a few new plugins

--- _config.ts
+++ _config.ts
@@ -9,6 +9,9 @@ import multilanguage from "lume/plugins/multilanguage.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import vento from "lume/plugins/vento.ts";
+import checkUrls from "lume/plugins/check_urls.ts";
+import googleFonts from "lume/plugins/google_fonts.ts";
+import icons from "lume/plugins/icons.ts";
 
 // Remark plugins
 import remark from "lume/plugins/remark.ts";
@@ -24,10 +27,15 @@ const site = lume({ src: "./src" })
   .use(remark({ remarkPlugins: [emoji, a11yEmoji, smartyPants] }))
   .use(esbuild())
   .use(date())
+  .use(googleFonts({
+    fonts:
+      "https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&family=Atkinson+Hyperlegible+Next:ital,wght@0,200..800;1,200..800&display=swap",
+  }))
   .use(tailwindcss({
     includes: "_styles",
     minify: true,
   }))
+  .use(icons())
   .use(inline())
   .use(vento())
   .use(extractDate())
@@ -36,6 +44,7 @@ const site = lume({ src: "./src" })
     languages: ["en", "zh"],
     defaultLanguage: "en",
   }))
+  .use(checkUrls())
   .use(feed({
     output: ["/posts.rss", "/posts.json"],
     query: "post",
